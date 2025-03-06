f0b7d167ef9e998820153c68552547663c187e06
FullStackOverFlow77 <NightFace777@gmail.com>
2025-03-06 16:51:16 +0000

    Save2

---
+++ Estrutura sequencial/teste.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class teste {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in); 
+
+        String s1,s2,s3; 
+
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("\nDados Digitados: "); 
+        System.out.println("1ª" + s1);
+        System.out.println("2ª" + s2);
+        System.out.println("3ª" + s3);
+
+        sc.close();
+    }
+}
