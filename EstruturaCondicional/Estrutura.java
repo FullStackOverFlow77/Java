7da8a55d291d7fcddf79bad7c325491bc46c5fa8
FullStackOverFlow77 <NightFace777@gmail.com>
2025-03-06 22:35:43 +0000

    save4

---
+++ EstruturaCondicional/Estrutura.java
@@ -0,0 +1,14 @@
+package EstruturaCondicional;
+public class Estrutura{
+    public static void main (String[]args) {
+        
+        int x = 5 ;
+        
+        System.out.println("Bom dia");
+        
+        if (x > 2) {
+            System.out.println("Boa tarde");
+            }
+        System.out.println("Boa noite");
+    }
+}
