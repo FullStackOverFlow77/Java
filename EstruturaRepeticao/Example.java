3a1aa1dc7bf5d6811b4856b3a9e051ae75013b69
FullStackOverFlow77 <NightFace777@gmail.com>
2025-03-08 00:07:11 +0000

    save6

--- EstruturaRepeticao/Example.java
+++ EstruturaRepeticao/Example.java
@@ -4,11 +4,15 @@ public class Example {
         Scanner sc = new Scanner (System.in);
 
         int x = sc.nextInt(); 
+        int soma = 0;
 
         while (x != 0){
+            soma += x ;
             x = sc.nextInt();
 
         }
+
+        System.out.println(soma);
         sc.close();
     }
 }
---
+++ EstruturaRepeticao/contagemregressiva.java
@@ -0,0 +1,8 @@
+public class contagemregressiva {
+    public static void main(String[] args) {
+        for (int i = 4; i >= 0; i--) {
+            System.out.println("Valor de i: " + i);
+        }
+        
+    }
+}
