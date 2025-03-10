f34cb56a50115698825d3ea8ba6bad8ca1c7f8cf
FullStackOverFlow77 <NightFace777@gmail.com>
2025-03-10 19:37:44 +0000

    ola

---
+++ tt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String x = sc.nextLine(); 
+        String y = sc.nextLine();
+
+       
+        System.out.println("Exiba " + x);  
+        System.out.println("Exiba " + y);  
+
+        sc.close();
+    }
+}
