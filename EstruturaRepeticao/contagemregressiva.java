ad76a7fcd970d417c7de1b3841407018db4e6281
FullStackOverFlow77 <NightFace777@gmail.com>
2025-04-10 15:51:32 +0000

    save7777

--- EstruturaRepeticao/contagemregressiva.java
+++
@@ -1,8 +0,0 @@
-public class contagemregressiva {
-    public static void main(String[] args) {
-        for (int i = 4; i >= 0; i--) {
-            System.out.println("Valor de i: " + i);
-        }
-        
-    }
-}
