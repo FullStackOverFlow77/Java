ac46ebddcfd04b0ab8b2111abdc05600036d127e
FullStackOverFlow77 <NightFace777@gmail.com>
2025-04-14 00:00:35 +0000

    meudeus

---
+++ testepessoal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class testepessoal {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        
+        for(int i = 0 ; i < 5 ; i ++){
+
+        System.out.println("Contagem Regressiva: " + i);
+        
+        }
+     
+        sc.close();
+        }
+  }
+
